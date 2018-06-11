4be862f3d922c61777d97123317140da9c2aeb25
jaehosung <jaehossung@gmail.com>
2018-06-12 00:50:34 +0900

    9blocks

--- App.js
+++ App.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {Text, Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
-  render() {
-    return (
-      <Text>Hello world!</Text>
-    );
-  }
+export default class ButtonBasics extends Component {
+	_onPressButton() {
+	}
+
+render() {
+	return (
+			<View style={styles.container}>
+			<View style={styles.alternativeLayoutButtonContainer}>
+			<Button
+			onPress={this._onPressButton}
+			title="ㅁ"
+			/>
+			<Button
+			onPress={this._onPressButton}
+			title="ㅁ"
+			/>
+			<Button
+			onPress={this._onPressButton}
+			title="ㅁ"
+			/>
+			</View>
+			<View style={styles.alternativeLayoutButtonContainer}>
+			<Button
+			onPress={this._onPressButton}
+			title="ㅁ"
+			/>
+			<Button
+			onPress={this._onPressButton}
+			title="ㅁ"
+				/>
+				<Button
+				onPress={this._onPressButton}
+			title="ㅁ"
+				/>
+				</View>
+				<View style={styles.alternativeLayoutButtonContainer}>
+				<Button
+				onPress={this._onPressButton}
+			title="ㅁ"
+				/>
+				<Button
+				onPress={this._onPressButton}
+			title="ㅁ"
+				/>
+				<Button
+				onPress={this._onPressButton}
+			title="ㅁ"
+				/>
+				</View>
+				</View>
+				);
 }
+}
+
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+justifyContent: 'center',
+},
+buttonContainer: {
+margin: 20
+},
+alternativeLayoutButtonContainer: {
+margin: 20,
+flexDirection: 'row',
+justifyContent: 'space-between'
+}
+})
+
